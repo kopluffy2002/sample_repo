bcd044fba7a890f329137e875667663776db55ce
kopluffy2002 <kopluffy2002@gmail.com>
2026-05-22 04:56:32 +0530

    env.yaml file change

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // ── Middleware ────────────────────────────────────────────────────────────────
-app.use(cors());
+
 app.use(express.json());
 app.use(bodyParser.json());
-app.use("/uploads", express.static("uploads"));
 app.use(express.static(path.join(__dirname, "public")));
 
 // ── HTTP + Socket.io ──────────────────────────────────────────────────────────
@@ -23,7 +22,7 @@ app.use(
   cors({
     origin: [
       "https://schedumingle-frontend-516427689972.asia-south1.run.app",
-      "http://localhost:4200", // for local dev
+      "http://localhost:4200",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
